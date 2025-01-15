9d5f37c2a69cac5017beac173b5b54bcc780ca0b
Akram Aouiche <akram-aouiche@hotmail.com>
2025-01-15 21:23:41 +0000

    First scrape with htmlagilitypack working.

--- WebScrapeLearning/Program.cs
+++ WebScrapeLearning/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using HtmlAgilityPack;
+
+String url = "https://weather.com/en-GB/weather/today/l/UKXX0085:1:UK?Goto=Redirected";
+var httpClient = new HttpClient();
+var html = httpClient.GetStringAsync(url).Result;
+var htmlDocument = new HtmlDocument(); //Used to parse, manipulate, and traverse HTML documents programmatically.
+htmlDocument.LoadHtml(html);
+
+var temperatureElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='CurrentConditions--tempValue--zUBSz']");
+var temperature = temperatureElement.InnerText.Trim();
+Console.WriteLine(temperature);
+
+
+
+Console.ReadKey();
