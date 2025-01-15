6499f84bc394538fdc5318f248e73de947f6e7f3
Akram Aouiche <akram-aouiche@hotmail.com>
2025-01-15 22:03:28 +0000

    Selecting multiple nodes and looping

--- WebScrapeLearning/Program.cs
+++ WebScrapeLearning/Program.cs
@@ -2,7 +2,8 @@
 
 using HtmlAgilityPack;
 
-String url = "https://weather.com/en-GB/weather/today/l/UKXX0085:1:UK?Goto=Redirected";
+//Weather website
+/*String url = "https://weather.com/en-GB/weather/today/l/UKXX0085:1:UK?Goto=Redirected";
 var httpClient = new HttpClient();
 var html = httpClient.GetStringAsync(url).Result;
 var htmlDocument = new HtmlDocument(); //Used to parse, manipulate, and traverse HTML documents programmatically.
@@ -14,6 +15,24 @@ Console.WriteLine(temperature);
 
 var airQualityElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='AirQualityText--severity--jiW+F']");
 var airQuality = airQualityElement.InnerText.Trim();
-Console.WriteLine(airQuality);
+Console.WriteLine(airQuality);*/
 
-Console.ReadKey();
+
+//Book website
+String url = "https://books.toscrape.com/";
+var httpClient = new HttpClient();
+var html = httpClient.GetStringAsync(url).Result;
+var htmlDocument = new HtmlDocument(); //Used to parse, manipulate, and traverse HTML documents programmatically.
+htmlDocument.LoadHtml(html);
+
+var listOfBooksElement = htmlDocument.DocumentNode.SelectNodes("//ol[@class='row']/li");
+foreach(var bookElement in listOfBooksElement)
+{
+    Console.WriteLine(bookElement.InnerText.Trim());
+}
+//Console.WriteLine(listOfBooksElement);
+
+
+
+
+Console.ReadLine();
